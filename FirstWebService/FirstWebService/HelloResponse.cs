173e467bd6636ef0f709438910793d7a2b3c83cd
Lee Francis Wilhelmsen <lee@sandviks.com>
2012-04-20 13:35:31 +0200

    Added ResponseStatus property for automatic exception handling

--- FirstWebService/FirstWebService/HelloResponse.cs
+++ FirstWebService/FirstWebService/HelloResponse.cs
@@ -1,7 +1,11 @@
 ﻿namespace FirstWebService
 {
+    using ServiceStack.ServiceInterface.ServiceModel;
+
     public class HelloResponse
     {
         public string Result { get; set; }
+
+        public ResponseStatus ResponseStatus { get; set; } //Automatic exception handling
     }
 }
