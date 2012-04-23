f96c3624497cd84a32e199f21542a770e04c9e0c
Lee Francis Wilhelmsen <lee@sandviks.com>
2012-04-23 14:42:36 +0200

    Returns a collection object from service. Used DataMemberAttribute to specify json property name

--- FirstWebService/FirstWebService/HelloResponse.cs
+++ FirstWebService/FirstWebService/HelloResponse.cs
@@ -1,10 +1,15 @@
 ﻿namespace FirstWebService
 {
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
     using ServiceStack.ServiceInterface.ServiceModel;
 
+    [DataContract]
     public class HelloResponse
     {
-        public string Result { get; set; }
+        [DataMember(Name = "name")]
+        public IList<ServiceItemObject> Result { get; set; }
 
         public ResponseStatus ResponseStatus { get; set; } //Automatic exception handling
     }
--- FirstWebService/FirstWebService/HelloService.cs
+++ FirstWebService/FirstWebService/HelloService.cs
@@ -1,12 +1,28 @@
 ﻿namespace FirstWebService
 {
+    using System.Collections.Generic;
+
     using ServiceStack.ServiceInterface;
 
     public class HelloService : ServiceBase<Hello>
     {
         protected override object Run(Hello request)
         {
-            return new HelloResponse { Result = "Hello, " + request.Name };
+            IList<ServiceItemObject> names = new List<ServiceItemObject>();
+            names.Add(new ServiceItemObject() { ItemName = "str1" });
+            names.Add(new ServiceItemObject() { ItemName = "str2" });
+            names.Add(new ServiceItemObject() { ItemName = "str3" });
+
+            return new HelloResponse { Result = names };
         }
+    }
+
+    public class ServiceItemObject
+    {
+        public ServiceItemObject()
+        {
+        }
+
+        public string ItemName { get; set; }
     }
 }
