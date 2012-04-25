47d1fbc592673ecfa945c3250bae984eead2863a
Lee Francis Wilhelmsen <lee@sandviks.com>
2012-04-25 12:13:01 +0200

    Added service description metadata

--- FirstWebService/FirstWebService/Hello.cs
+++ FirstWebService/FirstWebService/Hello.cs
@@ -1,5 +1,8 @@
 ﻿namespace FirstWebService
 {
+    using System.ComponentModel;
+
+    [Description("A simple hello world example to get familiar with the basics of the Service Stack framework!")]
     public class Hello
     {
         public string Name { get; set; }
